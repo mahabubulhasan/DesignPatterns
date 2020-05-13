5a1f2628a30c03e2d6ebe3a1bef2b9ceccdbb498
Mahabubul Hasan <codehasan@gmail.com>
2020-05-13 18:37:02 +0600

    builder pattern

---
+++ Builder/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Builder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var director = new Director();
+            var builder = new ConcreteBuilder();
+            director.Builder = builder;
+
+            Console.WriteLine("Standard basic product:");
+            director.buildMinimalViableProduct();
+            Console.WriteLine(builder.GetProduct().ListParts());
+
+            Console.WriteLine("Standard full featured product:");
+            director.buildFullFeaturedProduct();
+            Console.WriteLine(builder.GetProduct().ListParts());
+
+            // Remember, the Builder pattern can be used without a Director
+            // class.
+            Console.WriteLine("Custom product:");
+            builder.BuildPartA();
+            builder.BuildPartC();
+            Console.Write(builder.GetProduct().ListParts());
+        }
+    }
+
+    public class Director{
+
+        private IBuilder _builder;
+
+        public IBuilder Builder
+        {
+            set
+            {
+                _builder = value;
+            }
+        }
+
+        public void buildMinimalViableProduct()
+        {
+            this._builder.BuildPartA();
+        }
+
+        public void buildFullFeaturedProduct()
+        {
+            this._builder.BuildPartA();
+            this._builder.BuildPartB();
+            this._builder.BuildPartC();
+        }
+
+    }
+
+    public interface IBuilder
+    {
+        void BuildPartA();
+        void BuildPartB();
+        void BuildPartC();
+    }
+
+    public class ConcreteBuilder : IBuilder
+    {
+        private Product _product = new Product();
+
+        public ConcreteBuilder()
+        {
+            this.Reset();
+        }
+
+        public void Reset()
+        {
+            _product = new Product();
+        }
+
+        public void BuildPartA()
+        {
+            _product.Add("Part A");
+        }
+
+        public void BuildPartB()
+        {
+            _product.Add("Part B");
+        }
+
+        public void BuildPartC()
+        {
+            _product.Add("Part C");
+        }
+
+        public Product GetProduct()
+        {
+            Product result = _product;
+            this.Reset();
+            return result;
+        }
+    }
+
+    public class Product
+    {
+        private List<object> _parts = new List<object>();
+
+        public void Add(string part)
+        {
+            this._parts.Add(part);
+        }
+
+        public string ListParts()
+        {
+            string str = string.Empty;
+
+            for (int i = 0; i < this._parts.Count; i++)
+            {
+                str += this._parts[i] + ", ";
+            }
+
+            str = str.Remove(str.Length - 2); // removing last ",c"
+
+            return "Product parts: " + str + "\n";
+        }
+    }
+}
